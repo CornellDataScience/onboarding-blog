ba78e19190c4551079fb5d88fd98b0028dbb7496
dhan0779 <davidhan2021@gmail.com>
2021-11-13 16:45:22 -0500

    changed properties for tables

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql')
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'cornellde',
+  password: 'password',
   database: 'blog_db'
 })
 
@@ -12,21 +12,21 @@ connection.connect()
 // connection.query(`DROP TABLE blogs;`)
 
 connection.query(`CREATE TABLE users(
-  id INT,
-  name VARCHAR(255),
-  PRIMARY KEY(id)
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
   );`, function (err, rows, fields) {
   if (err) throw err
   console.log(err)
 })
 
 connection.query(`CREATE TABLE blogs(
-  id INT,
-  author_id INT,
-  date DATETIME,
-  title VARCHAR(255),
-  content LONGTEXT,
-  PRIMARY KEY(id),
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  author_id INT NOT NULL,
+  date DATE NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  subtitle LONGTEXT NOT NULL,
+  paper LONGTEXT NOT NULL,
+  content LONGTEXT NOT NULL,
   FOREIGN KEY(author_id) REFERENCES users(id)
   );`, function (err, rows, fields) {
   if (err) throw err
