fcb9497d4c0f681279b0730ccdce3a5cfc72dc92
Sirawut Chaixanien <sc2343@cornell.edu>
2021-11-13 15:09:58 -0500

    author html

--- server.js
+++ server.js
@@ -37,6 +37,28 @@ app.get("/", function (req, res) {
   });
 });
 
+app.get("/author", function (req, res) {
+  let posting = [
+    {
+      title: "Optimal Classification Trees Paper Summary & Analysis",
+      subtitle:
+        "Paper: https://link.springer.com/article/10.1007/s10994-017-5633-9\nDiscussion led by Peter Husisian & Julia Allen, Intelligent Systems Subteam",
+      content: "Example blog post content!\nHello World",
+    },
+    {
+      title:
+        "Do Better ImageNet Models Transfer Better? Paper Summary & Analysis",
+      subtitle:
+        "Paper: https://openaccess.thecvf.com/content_CVPR_2019/papers/Kornblith_Do_Better_ImageNet_Models_Transfer_Better_CVPR_2019_paper.pdf\nDiscussion led by Stephen Tse & Felix Hohne, Intelligent Systems Subteam",
+      content: "Example blog post content numero 2!\nHello World 2",
+    },
+  ];
+
+  res.render("pages/author", {
+    posts: posting,
+  });
+})
+
 const server = http.createServer(app);
 const port = 3000;
 server.listen(port);
